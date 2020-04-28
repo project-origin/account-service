08acd36f356ff3df9cf880e7d3171d50674c54d7
Jakob Kristensen <jakob@jakobk.dk>
2020-04-28 11:52:00 +0200

    Added SECRET to environment

--- src/origin/settings.py
+++ src/origin/settings.py
@@ -7,6 +7,7 @@ DEBUG = os.environ.get('DEBUG') in ('1', 't', 'true', 'yes')
 # -- Project -----------------------------------------------------------------
 
 PROJECT_NAME = 'Project Origin AccountService'
+SECRET = os.environ['SECRET']
 PROJECT_URL = os.environ['PROJECT_URL']
 LOGIN_CALLBACK_URL = f'{PROJECT_URL}/auth/login/callback'
 CORS_ORIGINS = os.environ['CORS_ORIGINS']
