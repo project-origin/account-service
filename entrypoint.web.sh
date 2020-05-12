ae312ebdc58a10aeb4fda7a279531b5367ec3127
Jakob Kristensen <jakob@jakobk.dk>
2020-05-12 15:51:30 +0200

    Removed name from Gunicorn

--- entrypoint.web.sh
+++ entrypoint.web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /app/migrations && pipenv run migrate || exit
-cd /app && pipenv run gunicorn -b 0.0.0.0:8085 origin:app --workers $WORKERS --worker-class gevent --worker-connections $WORKER_CONNECTIONS --name \"Account Service\"
+cd /app && pipenv run gunicorn -b 0.0.0.0:8085 origin:app --workers $WORKERS --worker-class gevent --worker-connections $WORKER_CONNECTIONS
