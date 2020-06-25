41d0266d9a6ee963e3e8e8a7d8d16f4c4e514ee5
Jakob Kristensen <jakob@jakobk.dk>
2020-06-25 11:07:11 +0200

    Renamed /compose to /ggo/compose and /transfer/get-transferred-amount to /transfer/get-total-amount

--- src/origin/urls.py
+++ src/origin/urls.py
@@ -14,18 +14,24 @@ urls = (
     ('/accounts', auth.GetAccounts()),
 
     ('/ggo', ggo.GetGgoList()),
+    ('/ggo/compose', ggo.ComposeGgo()),
     ('/ggo/summary', ggo.GetGgoSummary()),
     ('/ggo/get-total-amount', ggo.GetTotalAmount()),
-    ('/compose', ggo.ComposeGgo()),
 
     # Transfers
     ('/transfer/summary', ggo.GetTransferSummary()),
-    ('/transfer/get-transferred-amount', ggo.GetTransferredAmount()),
+    ('/transfer/get-total-amount', ggo.GetTransferredAmount()),
 
     # Webhooks
     ('/webhook/on-ggos-issued', ggo.OnGgosIssuedWebhook()),
     ('/webhook/on-meteringpoints-available', auth.OnMeteringPointsAvailableWebhook()),
     ('/webhook/on-ggo-received/subscribe', webhooks.Subscribe(WebhookEvent.ON_GGO_RECEIVED)),
     ('/webhook/on-ggo-received/unsubscribe', webhooks.Unsubscribe(WebhookEvent.ON_GGO_RECEIVED)),
+
+    # TODO
+    # Remove these once ExampleBackend is been changed to make use
+    # of /ggo/compose in stead (be for backwards compatibility)
+    ('/compose', ggo.ComposeGgo()),
+    ('/transfer/get-transferred-amount', ggo.GetTransferredAmount()),
 
 )
