24cd5f538e5c80fa6fed67ded85c2249040ded2d
Jakob Kristensen <jakob@jakobk.dk>
2020-08-25 12:27:57 +0200

    Fixed SOURCE_DIR settings which did not work in Docker

--- src/origin/settings.py
+++ src/origin/settings.py
@@ -7,7 +7,7 @@ __current_folder = os.path.split(__current_file)[0]
 
 PROJECT_DIR = os.path.abspath(os.path.join(__current_folder, '..', '..'))
 VAR_DIR = os.path.join(PROJECT_DIR, 'var')
-SOURCE_DIR = os.path.join(PROJECT_DIR, 'src')
+SOURCE_DIR = os.path.abspath(os.path.join(__current_folder, '..'))
 MIGRATIONS_DIR = os.path.join(SOURCE_DIR, 'migrations')
 ALEMBIC_CONFIG_PATH = os.path.join(MIGRATIONS_DIR, 'alembic.ini')
 
